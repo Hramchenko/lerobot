5699a91554b3d832617015395761be7b7ca996e7
Vitaliy Hramchenko <hramchenko@bk.ru>
2026-02-08 09:38:08 +0500

    dynamixel patch

--- src/lerobot/motors/dynamixel/dynamixel.py
+++ src/lerobot/motors/dynamixel/dynamixel.py
@@ -34,8 +34,49 @@ from .tables import (
 )
 
 PROTOCOL_VERSION = 2.0
-DEFAULT_BAUDRATE = 1_000_000
+#DEFAULT_BAUDRATE = 1_000_000
 DEFAULT_TIMEOUT_MS = 1000
+
+#********* DYNAMIXEL Model definition *********
+#***** (Use only one definition at a time) *****
+MY_DXL = 'X_SERIES'       # X330 (5.0 V recommended), X430, X540, 2X430
+# MY_DXL = 'MX_SERIES'    # MX series with 2.0 firmware update.
+# MY_DXL = 'PRO_SERIES'   # H54, H42, M54, M42, L54, L42
+# MY_DXL = 'PRO_A_SERIES' # PRO series with (A) firmware update.
+# MY_DXL = 'P_SERIES'     # PH54, PH42, PM54
+# MY_DXL = 'XL320'        # [WARNING] Operating Voltage : 7.4V
+
+
+# Control table address
+if MY_DXL == 'X_SERIES' or MY_DXL == 'MX_SERIES':
+    ADDR_TORQUE_ENABLE          = 64
+    ADDR_GOAL_POSITION          = 116
+    ADDR_PRESENT_POSITION       = 132
+    DXL_MINIMUM_POSITION_VALUE  = 0         # Refer to the Minimum Position Limit of product eManual
+    DXL_MAXIMUM_POSITION_VALUE  = 4095      # Refer to the Maximum Position Limit of product eManual
+    BAUDRATE                    = 57600
+elif MY_DXL == 'PRO_SERIES':
+    ADDR_TORQUE_ENABLE          = 562       # Control table address is different in DYNAMIXEL model
+    ADDR_GOAL_POSITION          = 596
+    ADDR_PRESENT_POSITION       = 611
+    DXL_MINIMUM_POSITION_VALUE  = -150000   # Refer to the Minimum Position Limit of product eManual
+    DXL_MAXIMUM_POSITION_VALUE  = 150000    # Refer to the Maximum Position Limit of product eManual
+    BAUDRATE                    = 57600
+elif MY_DXL == 'P_SERIES' or MY_DXL == 'PRO_A_SERIES':
+    ADDR_TORQUE_ENABLE          = 512        # Control table address is different in DYNAMIXEL model
+    ADDR_GOAL_POSITION          = 564
+    ADDR_PRESENT_POSITION       = 580
+    DXL_MINIMUM_POSITION_VALUE  = -150000   # Refer to the Minimum Position Limit of product eManual
+    DXL_MAXIMUM_POSITION_VALUE  = 150000    # Refer to the Maximum Position Limit of product eManual
+    BAUDRATE                    = 57600
+elif MY_DXL == 'XL320':
+    ADDR_TORQUE_ENABLE          = 24
+    ADDR_GOAL_POSITION          = 30
+    ADDR_PRESENT_POSITION       = 37
+    DXL_MINIMUM_POSITION_VALUE  = 0         # Refer to the CW Angle Limit of product eManual
+    DXL_MAXIMUM_POSITION_VALUE  = 1023      # Refer to the CCW Angle Limit of product eManual
+    BAUDRATE                    = 1000000   # Default Baudrate of XL-320 is 1Mbps
+
 
 NORMALIZED_DATA = ["Goal_Position", "Present_Position"]
 
